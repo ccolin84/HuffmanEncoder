a072acf82bc8e739808b0f099eb30a158359444d
ccolin84 <ccolin84@gmail.com>
2019-01-24 19:49:21 -0500

    refactor HuffmanEncoder and add encoding the input message to a string

--- src/main/java/com/colin_crawford/huffman_encoding/huffman_encoding/HuffmanEncoder.java
+++ src/main/java/com/colin_crawford/huffman_encoding/huffman_encoding/HuffmanEncoder.java
@@ -1,15 +1,39 @@
 package com.colin_crawford.huffman_encoding.huffman_encoding;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class HuffmanEncoder {
-	public Map<Character, String> encode(String message) {
-		HuffmanNode[] frequencies = this.getFrequencyValueNodes(message);
-		HuffmanTree huffmanTree = new HuffmanTree(frequencies);
-		return this.getCodesFromTree(huffmanTree);
+	private final HuffmanTree huffmanTree;
+	private final Map<Character, String> charEncodings;
+	private final String encodedMessage;
+
+	HuffmanEncoder(String message) {
+		this(message, new HuffmanTree(message));
+	}
+
+	HuffmanEncoder(String message, HuffmanTree huffmanTree) {
+		this.huffmanTree = huffmanTree;
+		this.charEncodings = this.getCodesFromTree(huffmanTree);
+		this.encodedMessage = this.encodeMessage(message);
+	}
+
+	public Map<Character, String> getCharEncodings() {
+		return this.charEncodings;
+	}
+
+	public String getEncodedMessage() {
+		return this.encodedMessage;
+	}
+
+	public HuffmanTree getHuffmanTree() {
+		return this.huffmanTree;
+	}
+
+	private String encodeMessage(String message) {
+		StringBuilder sb = new StringBuilder();
+		CharacterIterable.of(message).forEach(c -> sb.append(this.charEncodings.get(c)));
+		return sb.toString();
 	}
 
 	private Map<Character, String> getCodesFromTree(HuffmanTree tree) {
@@ -19,13 +43,6 @@ public class HuffmanEncoder {
 		return codes;
 	}
 
-	private HuffmanNode[] getFrequencyValueNodes(String message) {
-		Iterable<Character> messageChars = this.getCharsFromMessage(message);
-		Map<Character, Integer> frequencies = new FrequencyCounter<Character>(messageChars).getFrequencies();
-		return frequencies.entrySet().stream().map(entry -> new HuffmanValueNode(entry.getValue(), entry.getKey()))
-				.toArray(HuffmanNode[]::new);
-	}
-
 	private void getCodesFromHuffmanNode(Map<Character, String> codes, String branchPath, HuffmanNode node) {
 		if (node instanceof HuffmanValueNode) {
 			codes.put(((HuffmanValueNode) node).getValue(), branchPath);
@@ -33,13 +50,5 @@ public class HuffmanEncoder {
 			this.getCodesFromHuffmanNode(codes, branchPath + "0", ((HuffmanCombinedNode) node).getLeft());
 			this.getCodesFromHuffmanNode(codes, branchPath + "1", ((HuffmanCombinedNode) node).getRight());
 		}
-	}
-
-	private Iterable<Character> getCharsFromMessage(String message) {
-		List<Character> chars = new ArrayList<>(message.length());
-		for (Character c : message.toCharArray()) {
-			chars.add(c);
-		}
-		return chars;
 	}
 }
--- src/test/java/com/colin_crawford/huffman_encoding/huffman_encoding/HuffmanEncoderTest.java
+++ src/test/java/com/colin_crawford/huffman_encoding/huffman_encoding/HuffmanEncoderTest.java
@@ -10,21 +10,21 @@ import org.junit.jupiter.api.Test;
 class HuffmanEncoderTest {
 
 	@Test
-	void testEncodeEmptyMessage() {
+	void constructor_GivenAnEmptyString_ItShouldntEncodeAnyChars() {
 		String message = "";
-		HuffmanEncoder encoder = new HuffmanEncoder();
+		HuffmanEncoder encoder = new HuffmanEncoder(message);
 
-		Map<Character, String> codes = encoder.encode(message);
+		Map<Character, String> codes = encoder.getCharEncodings();
 
 		assertEquals(0, codes.size());
 	}
 
 	@Test
-	void testEncodeOneCharacterMessage() {
+	void constructor_GivenASingleCharString_ItShouldEncodeThatChar() {
 		String message = "a";
-		HuffmanEncoder encoder = new HuffmanEncoder();
+		HuffmanEncoder encoder = new HuffmanEncoder(message);
 
-		Map<Character, String> codes = encoder.encode(message);
+		Map<Character, String> codes = encoder.getCharEncodings();
 
 		assertEquals(1, codes.size());
 		assertTrue(codes.containsKey('a'));
@@ -32,11 +32,11 @@ class HuffmanEncoderTest {
 	}
 
 	@Test
-	void testEncodeTwoCharacterMessage() {
+	void constructor_GivenATwoCharString_ItShouldEncodeThoseChars() {
 		String message = "abb";
-		HuffmanEncoder encoder = new HuffmanEncoder();
+		HuffmanEncoder encoder = new HuffmanEncoder(message);
 
-		Map<Character, String> codes = encoder.encode(message);
+		Map<Character, String> codes = encoder.getCharEncodings();
 
 		assertEquals(2, codes.size());
 		assertTrue(codes.containsKey('a'));
@@ -46,11 +46,11 @@ class HuffmanEncoderTest {
 	}
 
 	@Test
-	void testEncodeThreeCharacterMessage() {
+	void constructor_GivenAThreeCharString_ItShouldEncodeThoseChars() {
 		String message = "acbcbc";
-		HuffmanEncoder encoder = new HuffmanEncoder();
+		HuffmanEncoder encoder = new HuffmanEncoder(message);
 
-		Map<Character, String> codes = encoder.encode(message);
+		Map<Character, String> codes = encoder.getCharEncodings();
 
 		assertEquals(3, codes.size());
 		assertTrue(codes.containsKey('a'));
@@ -62,11 +62,11 @@ class HuffmanEncoderTest {
 	}
 
 	@Test
-	void testEncodeFourCharacterMessage() {
+	void constructor_GivenAFourCharString_ItShouldEncodeThoseChars() {
 		String message = "abbccccddddd";
-		HuffmanEncoder encoder = new HuffmanEncoder();
+		HuffmanEncoder encoder = new HuffmanEncoder(message);
 
-		Map<Character, String> codes = encoder.encode(message);
+		Map<Character, String> codes = encoder.getCharEncodings();
 
 		assertEquals(4, codes.size());
 		assertTrue(codes.containsKey('a'));
@@ -77,5 +77,39 @@ class HuffmanEncoderTest {
 		assertEquals("00", codes.get('c'));
 		assertEquals("010", codes.get('b'));
 		assertEquals("011", codes.get('a'));
+	}
+
+	@Test
+	void constructor_GivenAnEmptyString_ItShouldProduceAnEmptyEncodedMessage() {
+		String message = "";
+		HuffmanEncoder encoder = new HuffmanEncoder(message);
+
+		String encodedMessage = encoder.getEncodedMessage();
+
+		assertEquals("", encodedMessage);
+	}
+
+	@Test
+	void constructor_GivenASingleCharString_ItShouldProduceAnEmptyEncodedMessage() {
+		String message = "aaaa";
+		HuffmanEncoder encoder = new HuffmanEncoder(message);
+
+		String encodedMessage = encoder.getEncodedMessage();
+
+		assertEquals("", encodedMessage);
+	}
+
+	@Test
+	void constructor_GivenANonEmptyString_ItShouldProduceAnEncodedMessage() {
+		String message = "abababa";
+		HuffmanEncoder encoder = new HuffmanEncoder(message);
+
+		Map<Character, String> codes = encoder.getCharEncodings();
+		String encodedMessage = encoder.getEncodedMessage();
+		StringBuilder sb = new StringBuilder();
+		CharacterIterable.of(message).forEach(c -> sb.append(codes.get(c)));
+		String expectedMessage = sb.toString();
+
+		assertEquals(expectedMessage, encodedMessage);
 	}
 }
